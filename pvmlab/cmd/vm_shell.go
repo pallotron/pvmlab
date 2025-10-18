808ffbb8caa979363c6caa8a580faad4081d7b60
Angelo Failla <pallotron@gmail.com>
2025-10-18 17:43:39 +0100

    typo fix

--- pvmlab/cmd/vm_shell.go
+++ pvmlab/cmd/vm_shell.go
@@ -57,12 +57,12 @@ var vmShellCmd = &cobra.Command{
 
 			provisionerPort := fmt.Sprintf("%d", provisioner.SSHPort)
 			var targetIP string
-if meta.IPv6 != "" {
-    targetIP = meta.IPv6
-} else {
-    targetIP = meta.IP
-}
-targetConnect := fmt.Sprintf("ubuntu@%s", targetIP)
+			if meta.IPv6 != "" {
+				targetIP = meta.IPv6
+			} else {
+				targetIP = meta.IP
+			}
+			targetConnect := fmt.Sprintf("ubuntu@%s", targetIP)
 			proxyCommand := fmt.Sprintf("ssh -i %s -p %s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %%h:%%p ubuntu@localhost", sshKeyPath, provisionerPort)
 
 			color.Cyan("i Connecting to target VM via provisioner on port %s...", provisionerPort)
